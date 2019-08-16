f2231fa978fa1ca9c44f836b773742b6914e2d14
zhengcheng <1358981620@qq.com>
2019-08-16 17:00:53 +0800

    第一次提交代码

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <leftNav></leftNav>
+    <div class="main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+import leftNav from '@/components/leftNav'
+export default {
+  name:'app',
+  components:{
+    leftNav
+  }
+}
+</script>
+<style>
+#app{
+  font-family: 'avenir',Arial, sans-serif;
+  text-align: center;
+  color:#2C3E50;
+  /* margin-top:60px; */
+}
+.main{
+  float:left;
+  width: 95%;
+  background-color: #EFF2F7;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
